a8dcd51aeee7473c6aa9588c8a3d17d76e013332
Eliott Desclodures <eliott.desclodures@hei.yncrea.fr>
2021-03-26 15:30:05 +0100

    Activity

--- app/src/main/java/com/example/myapplication/Calendrier.java
+++ app/src/main/java/com/example/myapplication/Calendrier.java
@@ -1,21 +1,34 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Calendrier extends AppCompatActivity{
     private static final String TAG = "Calendrier";
     private TextView textedate;
     private Button Boutondate;
+    private RecyclerView recyclerViewD,recyclerViewG;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,21 @@ public class Calendrier extends AppCompatActivity{
         Intent incomingIntent = getIntent();
         String date = incomingIntent.getStringExtra("date");
         textedate.setText(date);
+
+        recyclerViewD = findViewById(R.id.recyclerD);
+        recyclerViewG = findViewById(R.id.recyclerG);
+        recyclerViewD.setHasFixedSize(true);
+        recyclerViewG.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager1 = new LinearLayoutManager(this);
+        recyclerViewD.setLayoutManager(layoutManager1);
+        RecyclerView.LayoutManager layoutManager2 = new LinearLayoutManager(this);
+        recyclerViewG.setLayoutManager(layoutManager2);
+        RecyclerView.Adapter mAdapter = new MyAdapter();
+        recyclerViewG.setAdapter(mAdapter);
+        recyclerViewD.setAdapter(mAdapter);
+
+
+
         Boutondate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +64,39 @@ public class Calendrier extends AppCompatActivity{
             }
         });
 
+    }
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+        @Override
+        public MyAdapter.MyViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(R.layout.hour, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder( MyAdapter.MyViewHolder holder, int position) {
+            Hour hour=new Hour(9,2,date);
+            holder.setHour(hour);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 6;
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+
+            public MyViewHolder( View itemView) {
+                super(itemView);
+                LinearLayout linearLayout= itemView.findViewById(R.id.layout);
+                TextView hour = itemView.findViewById(R.id.hour);
+                TextView message = itemView.findViewById(R.id.message);
+            }
+            public void setHour(Hour hour){
+
+            }
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/myapplication/Hour.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import java.util.Date;
+
+public class Hour {
+    private int hour;
+    private int terrain_dispos;
+    private String date;
+    public Hour(int hour, int terrain_dispos, String date){
+        this.hour=hour;
+        this.terrain_dispos=terrain_dispos;
+        this.date=date;
+    }
+}
