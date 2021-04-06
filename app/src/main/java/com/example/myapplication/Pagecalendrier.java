7c105ff4cf6c62c67e46fe4c4c2bacb3bd82eb94
Antoine Benatoui <antoine.benatoui@hei.yncrea.fr>
2021-04-06 09:45:38 +0200

    pageCalendrier

--- app/src/main/java/com/example/myapplication/Pagecalendrier.java
+++ app/src/main/java/com/example/myapplication/Pagecalendrier.java
@@ -33,93 +33,91 @@ public class Pagecalendrier extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagecalendrier);
-        calendrier = (CalendarView) findViewById(R.id.calendarView);
-        calendrier.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
-                Log.d(TAG, "onSelectedDayChange : dd/mm/yyyy: " + date);
-                Intent intent1 = new Intent(Pagecalendrier.this, Calendrier.class);
-                intent1.putExtra("date", date);
-                startActivity(intent1);
-            }
-        });
-
-        private void initWidgets ()
-        {
-            calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
-            monthYearText = findViewById(R.id.monthYear);
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        private void setMonthView ()
-        {
-            monthYearText.setText(monthYearFromDate(selectedDate));
-            ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
-
-            CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this::onItemClick);
-            RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
-            calendarRecyclerView.setLayoutManager(layoutManager);
-            calendarRecyclerView.setAdapter(calendarAdapter);
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        private ArrayList<String> daysInMonthArray (LocalDate date)
-        {
-            ArrayList<String> daysInMonthArray = new ArrayList<>();
-            YearMonth yearMonth = YearMonth.from(date);
-
-            int daysInMonth = yearMonth.lengthOfMonth();
-
-            LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-            int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
-
-            for (int i = 1; i <= 42; i++) {
-                if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
-                    daysInMonthArray.add("");
-                } else {
-                    daysInMonthArray.add(String.valueOf(i - dayOfWeek));
-                }
-            }
-            return daysInMonthArray;
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        private String monthYearFromDate (LocalDate date)
-        {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-            return date.format(formatter);
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        public void previousMonthAction (View view)
-        {
-            selectedDate = selectedDate.minusMonths(1);
-            setMonthView();
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        public void nextMonthAction (View view)
-        {
-            selectedDate = selectedDate.plusMonths(1);
-            setMonthView();
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.O)
-
-        public void onItemClick ( int position, String dayText)
-        {
-
-            if (!dayText.equals("")) {
-                String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Pagecalendrier.this, Calendrier.class);
-                startActivity(intent);
-            }
-        }
-
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
     }
+
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYear);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this::onItemClick);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private ArrayList<String> daysInMonthArray(LocalDate date)
+    {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
+            {
+                daysInMonthArray.add("");
+            }
+            else
+            {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return  daysInMonthArray;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void previousMonthAction(View view)
+    {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void nextMonthAction(View view)
+    {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+
+    public void onItemClick(int position, String dayText)
+    {
+
+        if(!dayText.equals(""))
+        {
+            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Pagecalendrier.this, Calendrier.class);
+            startActivity(intent);
+        }
+    }
+
 }
