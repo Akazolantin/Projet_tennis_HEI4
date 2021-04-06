fbbd8a365ff6397290e468c6e38bb0ea42503d6d
Asma Boutbagha <asma.boutbagha@student.yncrea.fr>
2021-04-06 10:54:38 +0200

    page administarteur 2 éme version

--- app/src/main/java/com/example/myapplication/Pageadministrateur.java
+++ app/src/main/java/com/example/myapplication/Pageadministrateur.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Pageadministrateur extends AppCompatActivity {
 
@@ -12,4 +16,31 @@ public class Pageadministrateur extends AppCompatActivity {
         setContentView(R.layout.activity_pageadministrateur);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.administrateur, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_deconnexion:
+                Intent Acitivity1 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(Acitivity1);
+                return true;
+            case R.id.menu_informations:
+                Intent Acitivity2 = new Intent(getApplicationContext(), Information.class);
+                startActivity(Acitivity2);
+                return true;
+            case R.id.menu_statistiques:
+                Intent Acitivity3 = new Intent(getApplicationContext(), Statistique.class);
+                startActivity(Acitivity3);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+    }
+}
 }
