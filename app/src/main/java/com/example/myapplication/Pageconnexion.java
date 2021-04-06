2939f3ed0fcf7bc0fa3fbaea1e80642bfbb736f3
Asma Boutbagha <asma.boutbagha@student.yncrea.fr>
2021-04-06 10:37:37 +0200

    page administrateur

--- app/src/main/java/com/example/myapplication/Pageconnexion.java
+++ app/src/main/java/com/example/myapplication/Pageconnexion.java
@@ -27,8 +27,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.common.api.Response;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
